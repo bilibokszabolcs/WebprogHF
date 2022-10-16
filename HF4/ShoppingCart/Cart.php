8c602b129f4512ed7fdc4db91158f1ea696bb883
bilibokszabolcs <bilibokszabolcs1@gmail.com>
2022-10-16 12:02:38 +0300

    HF4

---
+++ HF4/ShoppingCart/Cart.php
@@ -0,0 +1,64 @@
+<?php
+
+//include "autoloader.php";
+namespace ShoppingCart;
+
+class Cart
+{
+
+    private array $items = [];
+
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function setItems(array $items): void
+    {
+        $this->items = $items;
+    }
+
+    public function addProduct(Product $product, int $quantity): CartItem
+    {
+        $cartItem = new CartItem($product, $quantity);
+
+        if (in_array($cartItem, $this->items)) {
+
+            $oldQuantity = $this->items[$product->getId()]->getQuantity();
+
+            if ($oldQuantity + $quantity < $product->getAvailableQuantity()) {
+                $this->items[$product->getId()]->setQuantity($oldQuantity + $quantity);
+            }
+        } else {
+            $this->items[$product->getId()] = $cartItem;
+        }
+        return $cartItem;
+    }
+
+    public function removeProduct(Product $product)
+    {
+        $this->items[$product->getId()]->decreaseQuantity();
+
+    }
+
+    public function getTotalQuantity(): int
+    {
+        $totalQuantity = 0;
+
+        foreach ($this->items as $item) {
+            $totalQuantity = $totalQuantity + $item->getQuantity();
+        }
+        return $totalQuantity;
+    }
+
+    public function getTotalSum(): float
+    {
+        $totalSum = 0;
+
+        foreach ($this->items as $item) {
+            $totalSum = $totalSum + ($item->getQuantity() * $item->getProduct()->getPrice());
+        }
+        return $totalSum;
+    }
+}
